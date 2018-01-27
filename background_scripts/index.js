8f2353c236c96235041dc418cdcc09e9e5fe68b4
Erik Price <github@erikprice.net>
2018-01-27 01:18:31 -0800

    Start normalizing garbage data

--- background_scripts/index.js
+++ background_scripts/index.js
@@ -1,25 +1,100 @@
 import WAE from 'web-auto-extractor';
+import he from 'he';
 
 const TAB_RECIPE_MAP = {};
 
+// Mapping of ASCII encoded fraction to unicode.
+const FRACT_MAP = {
+    '1/2': '½',
+    '1/3': '⅓',
+    '2/3': '⅔',
+    '1/4': '¼',
+    '3/4': '¾',
+    // TODO: Finish this list later...
+};
 
 browser.pageAction.onClicked.addListener((e) => {
     browser.tabs.update({
         url: 'build/views/recipe.html'
     }).catch(e => {
-            console.error('FAILED to inject script:', e);
-        });
+        console.error('FAILED to inject script:', e);
+    });
 });
 
+
+// TODO: should we persist data rather than making everything ephemeral?
 browser.tabs.onRemoved.addListener((tabId) => {
     delete TAB_RECIPE_MAP[tabId];
 });
+
+
+function sanitizeString(str) {
+    // Strip out HTML entities
+    str = he.decode(str);
+
+    // Sometimes HTML tags end up in the text.
+    str = str.replace(/<(\/)?(p|div|span|b|i|em)>/g, '');
+
+    // Convert fractions into their unicode equivalent, falling back
+    // to the FRACTION character (U+2044).
+    str = str.replace(/(\d+)\/(\d+)/g, (m, n, d) => FRACT_MAP[m] || `${n}⁄${d}`);
+
+    return str;
+}
+
+
+function normalizeRecipe(recipe) {
+    console.log('recipe dirty', recipe);
+
+    let clean = {
+        name: recipe.name || 'An untitled recipe',
+        description: recipe.description,
+        ingredients: recipe.recipeIngredient || recipe.ingredients || [],
+        full: recipe
+    };
+
+    // Seems relatively common to have blank items in the list;
+    clean.ingredients = clean.ingredients
+        .map(i => i.trim())
+        .filter(i => i !== "");
+
+    if (typeof recipe.recipeInstructions === 'string') {
+        clean.instructionText = recipe.recipeInstructions;
+    }
+
+    if (Array.isArray(recipe.recipeInstructions)) {
+        clean.instructionList = recipe.recipeInstructions.map((inst, idx) => {
+            // Sometimes the instruction list includes a number
+            // prefix, strip that out.
+
+            return inst.replace(/^(\d+)\.?\s*/, (match, num) => {
+                if (num === idx + 1) return '';
+
+                return match;
+            });
+        });
+    }
+
+    // Remove the junk from the strings.
+    Object.keys(clean).forEach(k => {
+        if (typeof clean[k] === 'string') {
+            clean[k] = sanitizeString(clean[k]);
+        } else if (Array.isArray(clean[k])) {
+            clean[k] = clean[k].map(v => sanitizeString(v));
+        }
+    });
+
+    console.log('recipe cleaned:', clean);
+
+    return clean;
+}
+
 
 browser.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     console.log('sender.tab->', sender.tab);
 
     function setRecipeData(data) {
-        TAB_RECIPE_MAP[sender.tab.id] = data;
+        TAB_RECIPE_MAP[sender.tab.id] = normalizeRecipe(data);
         browser.pageAction.show(sender.tab.id);
     }
 
--- views/js/recipe.js
+++ views/js/recipe.js
@@ -18,25 +18,29 @@ function viewMeta(recipe) {
 }
 
 function viewIngredients(recipe) {
-    const ingredients = (recipe.recipeIngredient || recipe.ingredients);
-    return h('ul', {}, ingredients.map(i => {
+    return h('ul', {}, recipe.ingredients.map(i => {
         return h('li', {}, i);
     }));
 }
 
 function viewInstructions(recipe) {
-    if (typeof recipe.recipeInstructions === 'string') {
-        return h('p', {}, recipe.recipeInstructions);
+    if (recipe.instructionText) {
+        return h('p', {}, recipe.instructionText);
     }
 
-    return h('ol', {}, (recipe.recipeInstructions || []).map(i => {
-        return h('li', {className: 'instructions'}, i);
-    }));
+    if (recipe.instructionList) {
+        return h('ol', {}, recipe.instructionList.map(i => {
+            return h('li', {className: 'instruction'}, i);
+        }));
+    }
+
+    return h('p', {}, 'This recipe didn\'t include any instructions :(');
 }
 
 
 browser.runtime.sendMessage({kind: 'request-recipe'})
     .then(recipe => {
         console.log('Recipe -> ', recipe);
+        document.title = `${recipe.name} :: Recipe Thing`;
         const main = app(recipe, actions, view, document.body);
     });
