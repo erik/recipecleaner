fd92b43b8d76f8ae1b29b242ac83926e42dad881
Erik Price <github@erikprice.net>
2018-06-01 16:36:21 -0700

    Clean up options code, persist to storage

--- src/js/options.js
+++ src/js/options.js
@@ -6,7 +6,7 @@ const THEMES = {
         '--font-stack': 'Charter, Optima, Georgia, serif',
     },
     SANS_SERIF: {
-        '--font-stack': 'Avenir Next, Avenir, Helvetica, sans-serif'
+        '--font-stack': 'Avenir Next, Avenir, Helvetica, sans-serif',
     },
     DARK: {
         '--background-color': '#222',
@@ -14,11 +14,15 @@ const THEMES = {
         '--info-text-color': '#eee',
     },
     LIGHT: {
-        '--background-color': '#FFF',
+        '--background-color': '#fff',
         '--base-text-color': '#333',
         '--info-text-color': '#222',
     }
 };
+
+
+const BUG_REPORT_LINK = 'https://goo.gl/forms/bsr8RJJoeiXDKJqo2';
+const STORAGE_KEY = 'options';
 
 
 // Mapping of selector => click handler
@@ -27,72 +31,75 @@ const CLICK_HANDLERS = {
         const pane = document.querySelector('#options--pane');
         pane.classList.toggle('expanded');
     },
-    '#options--pane .theme':  (e) => {
-        console.log('--->'
-                    , e);
+
+    '#options--pane .theme': (e) => {
         const theme = e.target.dataset['theme'];
-        console.log('theme is', theme);
-        applyTheme(THEMES[theme]);
+        saveAndApplyOptions(THEMES[theme]);
     }
 };
 
-function getSavedOptions () {
-    return browser.storage.local.get({
-        'options.theme': THEMES['DEFAULT']
-    });
+function addClickHandlers (handlers) {
+    for (const selector in handlers) {
+        const handler = handlers[selector];
+
+        for (const node of document.querySelectorAll(selector)) {
+            node.addEventListener('click', handler);
+        }
+    }
 }
 
-function applyTheme (theme) {
+function getSavedOptions () {
+    return browser.storage.local
+        .get({[STORAGE_KEY]: THEMES['DEFAULT']})
+        .then(obj => obj[STORAGE_KEY]);
+}
+
+function saveOptions (options) {
+    return getSavedOptions()
+        .then(saved => browser.storage.local.set({
+            [STORAGE_KEY]: Object.assign(saved, options)
+        }));
+}
+
+function saveAndApplyOptions (theme) {
     console.log('APPLY THEME', theme);
     for (let key in theme) {
         if (theme[key] !== null) {
             document.body.style.setProperty(key, theme[key]);
         }
     }
+
+    return saveOptions(theme);
 }
 
 
-function renderOptionsPane () {
+function renderOptionsList () {
     const options = Object.keys(THEMES)
-          .map(el => `<li class="theme" data-theme="${el}">${el}</li>`)
-          .join('\n');
+        .map(el => `<li class="theme" data-theme="${el}">${el}</li>`)
+        .join('\n');
 
     return `<ul> ${ options } </ul>`;
 }
 
 
-function render() {
+function renderOptions () {
     return `
         <div>
             <div id="options--toggle"> OPTIONS </div>
             <div id="options--pane">
-                ${ renderOptionsPane() }
-
+                ${ renderOptionsList() }
                 <hr />
-
-                <a target="_blank" href="https://goo.gl/forms/bsr8RJJoeiXDKJqo2">
-                  Report a bug.
-                </a>
+                <a target="_blank" href="${BUG_REPORT_LINK}">Report a bug.</a>
             </div>
         </div>
     `;
 }
 
+getSavedOptions()
+    .then(opts => saveAndApplyOptions(opts))
+    .then(() => {
+        const node = document.querySelector('#options');
+        node.innerHTML = renderOptions();
 
-
-getSavedOptions().then(opts => {
-    applyTheme(opts['options.theme']);
-
-    const node = document.querySelector('#options');
-    node.innerHTML = view();
-
-    for (let selector in CLICK_HANDLERS) {
-        let handler = CLICK_HANDLERS[selector];
-
-        console.log('installing selector: ', selector);
-
-        for (let node of document.querySelectorAll(selector)) {
-            node.addEventListener('click', handler);
-        }
-    }
-});
+        addClickHandlers(CLICK_HANDLERS);
+    });
