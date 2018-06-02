16d3b5b781dc8dc697184e65a2c731247a9d96cb
Erik Price <github@erikprice.net>
2018-06-01 17:41:37 -0700

    Yank out babel

--- webpack.config.js
+++ webpack.config.js
@@ -24,11 +24,6 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
                 test: /\.css$/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
