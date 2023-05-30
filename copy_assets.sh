f14dfbcaa2ecb348054d86252075990c9374e09c
emdash <dotsony@gmail.com>
2023-05-30 15:36:58 -0700

    Fix issues with build script and url paths

--- copy_assets.sh
+++ copy_assets.sh
@@ -6,33 +6,25 @@ set -eu
 
 SRC_DIR=./src/
 BUILD_DIR=./addon/
-
-JS_VENDOR_FILE="$BUILD_DIR/js/vendor.js"
+JS_VENDOR_FILE="${BUILD_DIR}/js/vendor.js"
 VENDORED_JS="webextension-polyfill/dist/browser-polyfill.min.js"
+ASSETS="css icons images html manifest.json"
 
-ASSET_DIRS="css icons images"
-TOP_LEVEL_ASSETS="html/ manifest.json"
+mkdir -p "${BUILD_DIR}"
 
 echo 'copying asset directories'
-for d in $ASSET_DIRS; do
-    echo "  $d"
-    [[ -d "$BUILD_DIR/$d" ]] || mkdir -p "$BUILD_DIR/$d"
-    cp -r "$SRC_DIR/$d/" "$BUILD_DIR/$d/"
-done
-
-echo 'copying top level assets'
-for asset in $TOP_LEVEL_ASSETS; do
+for asset in $ASSETS; do
     echo "  $asset"
-    cp -r "$SRC_DIR/$asset" "$BUILD_DIR"
+    cp -r "${SRC_DIR}/${asset}" "${BUILD_DIR}"
 done
 
-[[ -d "$BUILD_DIR/js/" ]] || mkdir -p "$BUILD_DIR/js"
-:> "$JS_VENDOR_FILE"
+mkdir -p "${BUILD_DIR}/js"
+:> "${JS_VENDOR_FILE}"
 
 echo 'bundling vendored js files...'
 for vendor in $VENDORED_JS; do
-    echo "  $vendor"
-    cat "node_modules/$vendor" >> "$JS_VENDOR_FILE"
+    echo "  ${vendor}"
+    cat "node_modules/${vendor}" >> "${JS_VENDOR_FILE}"
 done
 
 echo 'done'
