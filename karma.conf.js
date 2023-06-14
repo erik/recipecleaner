8152c2a37e0834ebe90c2f3c1348b0fec0bd3165
emdash <dotsony@gmail.com>
2023-06-14 11:58:08 -0700

    remove karma-rollup-preprocessor

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,3 @@
-const resolve = require('rollup-plugin-node-resolve');
-const commonjs = require('rollup-plugin-commonjs');
-const globals = require('rollup-plugin-node-globals');
-const builtins = require('rollup-plugin-node-builtins');
-
-
 module.exports = function(config) {
     config.set({
         frameworks: ['mocha'],
@@ -12,31 +6,6 @@ module.exports = function(config) {
         files: [
             {pattern: 'test/*.js', watched: false}
         ],
-
-        preprocessors: {
-            'test/*.js': ['rollup']
-        },
-
-        rollupPreprocessor: {
-            output: {
-                format: 'es',
-                name: 'recipecleaner',
-                sourcemap: 'inline',
-                globals: ['chrome', 'browser'],
-            },
-            plugins: [
-                resolve({
-                    preferBuiltins: true,
-                    module: true,
-                    browser: true,
-                }),
-                commonjs({
-                    include: 'node_modules/**'
-                }),
-                globals(),
-                builtins(),
-            ]
-        },
 
         reporters: ['mocha'],
 
