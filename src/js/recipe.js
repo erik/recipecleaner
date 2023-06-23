b901e113d115cdfe620fa25df3c14bb43ad9bcd1
emdash <dotsony@gmail.com>
2023-06-22 17:22:47 -0700

    Allow saving in HTML, JSON, and Plain Text

--- src/js/recipe.js
+++ src/js/recipe.js
@@ -2,15 +2,27 @@ import extension from '/js/extension.js';
 import { addClickHandlers } from '/js/util.js';
 import { renderRecipe, renderError } from '/js/render_recipe.js';
 
-// Capture the page's HTML.
-//
-// Save page as is broken here, since it saves the page source, but
-// not the dynamic content.
+// Capture the recipe in the given format.
+function getSaveData(format) {
+  switch (format) {
+    case "HTML": return document.documentElement.outerHTML;
+    case "JSON": return JSON.stringify(gRecipe, null, 2);
+    case "Text": return document.documentElement.outerText;
+  }
+  throw new Error(`Invalid format: ${format}`);
+}
+
+// Handle user clicking the save button.
 async function onSaveClicked() {
   // create a data URL with the page content.
-  const blob = new Blob([document.documentElement.outerHTML]);
+  const format = document.getElementById("format").value;
+  const blob = new Blob([getSaveData(format)]);
   const url = URL.createObjectURL(blob);
-  browser.downloads.download({url, saveAs: true});
+  browser.downloads.download({
+    url,
+    filename: `${gRecipe.name}.${format.toLowerCase()}`,
+    saveAs: true
+  });
 }
 
 // Mapping of selector => click handler
@@ -45,16 +57,16 @@ const CLICK_HANDLERS = {
 
 const params = new URLSearchParams(window.location.search);
 const recipeId = decodeURI(params.get('recipeId') || 'no id');
+let gRecipe = null;
 
 extension.storage.getLocal(recipeId).then(recipe => {
   const node = document.querySelector('#recipe');
-
   console.log('Recipe -> ', recipe);
-
   if (recipe) {
+    // cache recipe in a global so it can be saved later
+    gRecipe = recipe;
     document.title = `${recipe.name} :: RecipeCleaner`;
     node.appendChild(renderRecipe(recipe));
-
     addClickHandlers(CLICK_HANDLERS);
   } else {
     node.appendChild(renderError());
