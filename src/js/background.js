8cbcece1b86320543d8d27bf77e6e82707d1b030
emdash <dotsony@gmail.com>
2023-06-24 12:12:34 -0700

    Ensure that each recipe is only stored in the database once

--- src/js/background.js
+++ src/js/background.js
@@ -4,24 +4,31 @@ import sanitize  from '/js/sanitize.js';
 
 // Mapping of tab id -> recipe (not persisted to storage)
 const EPHEMERAL_TAB_MAP = {};
+const encoder = new TextEncoder();
 
+// create a sha256 hash of the given string, returned as a hex string
+async function hash(string) {
+  const data = encoder.encode(string);
+  const digest = await crypto.subtle.digest("SHA-256", data);
+  const bytes = Array.from(new Uint8Array(digest));
+  return bytes
+    .map(b => b.toString(16).padStart(2, "0"))
+    .join("");
+}
 
 // save recipe to local storage
+//
+// recipes are identified by the sha256 hash of their url. this
+// ensures that a given recipe is only stored once in the database.
 async function saveRecipe (recipe) {
-  const cleanName = recipe.name
-    .replace(/\s+/g, '-')
-    .replace(/[^\w-]/g, '');
-  const id = `${Date.now()}-${cleanName}`;
-  await extension.storage.setLocal(id, recipe);
+  await extension.storage.setLocal(await hash(recipe.url), recipe);
 }
 
 
-// When the user clicks on the page action icon, we redirect to the
-// cleaned up recipe.
-//
-// We delay storing the recipe until the user actually wants it.
-extension.pageAction.onClicked((tab) => {
-  const url = `/html/recipe.html?recipeId=${encodeURI(id)}`;
+// Show the cleaned recipe when the user clicks on the page action.
+extension.pageAction.onClicked(async tab => {
+  const id = await hash(EPHEMERAL_TAB_MAP[tab.id].url);
+  const url = `/html/recipe.html?recipeId=${id}`;
   return extension.tabs.create(url);
 });
 
